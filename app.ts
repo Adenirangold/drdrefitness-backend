7aeaa85975aaa18971e0d7b63db3764cb2fd5924
adenirangold <adeniranbayogold@gmail.com>
2025-02-22 23:02:05 +0100

    Recfactoring app.ts

--- app.ts
+++ app.ts
@@ -34,6 +34,7 @@ app.use(errorHandler);
 
 // connectDatabase();
 app.listen(process.env.PORT, async () => {
-  await sendWelcomeEmail("Drelifestylecompany@gmail.com", `Adeniran Gold`);
+  // await sendWelcomeEmail("adeniranbayogold@gmail.com", `Adeniran Gold`);
+  console.log(`Server running on port ${process.env.PORT}`);
   console.log(`Server running on port ${process.env.PORT}`);
 });
