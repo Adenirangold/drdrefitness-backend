adbc703c5bc2265e30d0393c00c6efc75a1c2017
adenirangold <adeniranbayogold@gmail.com>
2025-02-28 22:22:30 +0100

    Fixing auth error

--- app.ts
+++ app.ts
@@ -38,4 +38,5 @@ app.listen(process.env.PORT, async () => {
 
   console.log(`Server running on port ${process.env.PORT}`);
   console.log(`Server running on port ${process.env.PORT}`);
+  console.log(`Server running on port ${process.env.PORT}`);
 });
