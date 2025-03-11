5aa4b527af14dff115565fbfc337a24ab62cc70f
adenirangold <adeniranbayogold@gmail.com>
2025-03-11 20:36:04 +0100

    working on couples plan

--- app.ts
+++ app.ts
@@ -35,4 +35,5 @@ app.use(errorHandler);
 // connectDatabase();
 app.listen(process.env.PORT, async () => {
   console.log(`Server running on port ${process.env.PORT}`);
+  console.log(`Server running on port ${process.env.PORT}`);
 });
