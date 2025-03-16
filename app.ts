4e126efcee3cbf052a66478dbee0fae5511fdf69
adenirangold <adeniranbayogold@gmail.com>
2025-03-16 23:05:24 +0100

    Refactoring app.ts

--- app.ts
+++ app.ts
@@ -37,4 +37,5 @@ app.use(errorHandler);
 app.listen(process.env.PORT, async () => {
   console.log(`Server running on port ${process.env.PORT}`);
   console.log(`Server running on port ${process.env.PORT}`);
+  console.log(`Server running on port ${process.env.PORT}`);
 });
