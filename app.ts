49c303889a5ce6116f0f2eb33a4d23408959c6ba
adenirangold <adeniranbayogold@gmail.com>
2025-03-17 16:26:57 +0100

    finished couple subscription

--- app.ts
+++ app.ts
@@ -36,4 +36,5 @@ app.use(errorHandler);
 // connectDatabase();
 app.listen(process.env.PORT, async () => {
   console.log(`Server running on port ${process.env.PORT}`);
+  console.log(`Server running on port ${process.env.PORT}`);
 });
