84a14462f536ae75acd3f186ba390a64210686f5
adenirangold <adeniranbayogold@gmail.com>
2025-04-06 22:08:13 +0100

    Refactoring app.ts

--- app.ts
+++ app.ts
@@ -50,4 +50,5 @@ connectDatabase();
 app.listen(process.env.PORT, async () => {
   console.log(`Server running on port ${process.env.PORT}`);
   console.log(`Server running on port ${process.env.PORT}`);
+  console.log(`Server running on port ${process.env.PORT}`);
 });
