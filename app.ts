189bdd822ce9a0b6fe2ed9c0c1053f43da68f648
adenirangold <adeniranbayogold@gmail.com>
2025-04-27 22:35:12 +0100

    Recfactoring

--- app.ts
+++ app.ts
@@ -49,6 +49,6 @@ app.use(errorHandler);
 
 connectDatabase();
 app.listen(process.env.PORT, async () => {
-  // console.log(`Server running on port ${process.env.PORT}`);
+  console.log(`Server running on port ${process.env.PORT}`);
 });
 // seedDatabase();
