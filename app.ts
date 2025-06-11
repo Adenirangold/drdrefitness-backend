aaf8fd5b5e9705c4b22821e0bf7ba21847bd195c
adenirangold <adeniranbayogold@gmail.com>
2025-06-11 23:23:49 +0100

    Restructured app.ts

--- app.ts
+++ app.ts
@@ -72,6 +72,5 @@ setupSocket(io);
 app.listen(process.env.PORT, async () => {
   console.log(`Server running on port ${process.env.PORT}`);
   console.log(`Server running on port ${process.env.PORT}`);
-  console.log(`Server running on port ${process.env.PORT}`);
 });
 // seedDatabase();
