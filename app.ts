c26ef0b994c0a64363ca35597f60925db0e9e85a
adenirangold <adeniranbayogold@gmail.com>
2025-07-01 23:13:16 +0100

    refactored app.ts

--- app.ts
+++ app.ts
@@ -76,5 +76,9 @@ export const io = setupSocket(ioServer);
 connectDatabase();
 server.listen(process.env.PORT, async () => {
   console.log(`Server running on port ${process.env.PORT}`);
+  console.log(`Server running on port ${process.env.PORT}`);
+  console.log(`Server running on port ${process.env.PORT}`);
+  console.log(`Server running on port ${process.env.PORT}`);
+  console.log(`Server running on port ${process.env.PORT}`);
 });
 // seedDatabase();
