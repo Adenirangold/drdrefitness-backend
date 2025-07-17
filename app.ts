852cb26106efce31fb75f819cb8236456528acc7
adenirangold <adeniranbayogold@gmail.com>
2025-07-17 23:36:21 +0100

    refactoring app.ts

--- app.ts
+++ app.ts
@@ -87,6 +87,5 @@ server.listen(process.env.PORT, async () => {
   console.log(`Server running on port ${process.env.PORT}`);
   console.log(`Server running on port ${process.env.PORT}`);
   console.log(`Server running on port ${process.env.PORT}`);
-  console.log(`Server running on port ${process.env.PORT}`);
 });
 // seedDatabase();
