bb5d4b5de4217537f61ba35f792c438ccf14d74d
adenirangold <adeniranbayogold@gmail.com>
2025-10-10 21:46:04 +0300

    Finished auth

--- app.ts
+++ app.ts
@@ -88,6 +88,5 @@ server.listen(process.env.PORT, async () => {
   console.log(`Server running on port ${process.env.PORT}`);
   console.log(`Server running on port ${process.env.PORT}`);
   console.log(`Server running on port ${process.env.PORT}`);
-  console.log(`Server running on port ${process.env.PORT}`);
 });
 // seedDatabase();
