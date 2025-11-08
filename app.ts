38986082b43e8d463c44a19893cb1471c1ff40a5
adenirangold <adeniranbayogold@gmail.com>
2025-11-08 21:53:42 +0200

    added schema

--- app.ts
+++ app.ts
@@ -85,5 +85,9 @@ connectDatabase();
 
 server.listen(process.env.PORT, async () => {
   console.log(`Server running on port ${process.env.PORT}`);
+  console.log(`Server running on port ${process.env.PORT}`);
+  console.log(`Server running on port ${process.env.PORT}`);
+  console.log(`Server running on port ${process.env.PORT}`);
+  console.log(`Server running on port ${process.env.PORT}`);
 });
 // seedDatabase();
