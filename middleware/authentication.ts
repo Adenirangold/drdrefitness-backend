8ce20c05bc6940d642e2fdd2cd579fb9f542f07e
adenirangold <adeniranbayogold@gmail.com>
2026-01-03 00:56:20 +0200

    changed role hierachy

--- middleware/authentication.ts
+++ middleware/authentication.ts
@@ -15,7 +15,7 @@ const hasAccess = (userRole: Role, requiredRole: Role) => {
     rolesHierarchy.indexOf(userRole) >= rolesHierarchy.indexOf(requiredRole)
     rolesHierarchy.indexOf(userRole) >= rolesHierarchy.indexOf(requiredRole)
     rolesHierarchy.indexOf(userRole) >= rolesHierarchy.indexOf(requiredRole)
-    rolesHierarchy.indexOf(userRole) >= rolesHierarchy.indexOf(requiredRole)
+ 
 
     
   );
